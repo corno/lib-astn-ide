8f648daed0916bf40630d384bf05eb4f297d8813
corno <corno.astn.a@schraverus.com>
2023-02-19 16:54:02 +0100

    .

--- pareto/src/data/project.data.ts
+++ pareto/src/data/project.data.ts
@@ -2,12 +2,12 @@ import * as pd from 'pareto-core-data'
 
 import * as mproject from "lib-pareto-typescript-project/dist/submodules/project"
 
-const d = pd.wrapRawDictionary
+const d = pd.d
 
 import { $ as api } from "./api.data"
 import { $ as private_ } from "./submodules/private/api.data"
 
-export const $: mproject.T.Project = {
+export const $: mproject.T.Project<pd.SourceLocation> = {
     'author': "Corno",
     'description': "functions that can be used by an IDE to support ASTN",
     'license': "ISC",
